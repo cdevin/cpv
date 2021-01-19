6f95bce2b59c7b449c1b8cbae765ab9e4cd732ba
Coline Devin <coline@berkeley.edu>
2021-01-18 18:29:45 -0800

    Fixed eval env

--- crafting/gridworld/envs/__init__.py
+++ crafting/gridworld/envs/__init__.py
@@ -19,62 +19,26 @@ for name in policy_names:
         }
         )
 
-
+##################
+# Language Gym Registration
+##################
 register(
-    id='HammerWorld-v0',
-    entry_point='gridworld.envs.grid_affordance:HammerWorld',
-    kwargs={'res':3, 'visible_agent':False}
-)
-
-def placeholder_reward(init_state, final_state):
-    raise NotImplementedError
-
-
-register(
-    id='HammerWorld-PlaceholderReward-v0',
-    entry_point='gridworld.envs.grid_affordance:HammerWorld',
-    kwargs={'res':3, 'visible_agent':True, 'agent_centric':True,
-            'reward_function':placeholder_reward, 'batch_reward':True}
-)
-for name in policy_names:
-    register(
-        id='HammerWorld-PlaceholderReward-'+name+'-v0',
-        entry_point='gridworld.envs.grid_affordance:HammerWorld',
-        kwargs={'res':3, 'visible_agent':True, 'agent_centric':True, 
-                'reward_function':placeholder_reward, 'batch_reward':True,
-                'success_function':  TASK_EVAL_MAP[name], 'add_objects':policy_inputs[name],
-               }
-    )
-    
-for name in policy_names:
-    register(
-        id='HammerWorld-Delta-'+name+'-v0',
-        entry_point='gridworld.envs.grid_affordance:HammerWorld',
-        kwargs={'res':3, 'visible_agent':True, 'agent_centric':False,
-                'reward_function':TASK_EVAL_MAP[name], 
-                'success_function':  TASK_EVAL_MAP[name], 'add_objects':policy_inputs[name],
-                'goal_dim': 64,
-               }
-    )
-register(
-    id='ResettingWorld-v0',
-    entry_point='gridworld.envs.resetting_affordance:ResettingHammerWorld',
-    kwargs={'res':3, 'visible_agent':True, 'agent_centric':False, 'success_function':  TASK_EVAL_MAP['EatBreadPolicy'],
-            'reward_function':TASK_EVAL_MAP['EatBreadPolicy'], 'batch_reward':False}
+    id='Crafting-Atomic-v0',
+    entry_point='gridworld.envs.babayai_wrapper:GymCraftingEnv',
+    kwargs={'num_tasks': 1}
 )
 register(
-    id='GoalAffordanceWorld-v0',
-    entry_point='gridworld.envs.goalenv_grid_affordance:GoalHammerWorld',
-    kwargs={'res':3, 'visible_agent':True, 'agent_centric':False,'batch_reward':False}
-)
-
-register(
-    id='GoalResettingWorld-v0',
-    entry_point='gridworld.envs.goal_resetting_affordance:GoalResettingHammerWorld',
-    kwargs={'res':3, 'visible_agent':True, 'agent_centric':False, 'batch_reward':False}
+    id='Crafting-2Tasks-v0',
+    entry_point='gridworld.envs.babayai_wrapper:GymCraftingEnv',
+    kwargs={'num_tasks': 2}
 )
 register(
-    id='IBCGoalResettingWorld-v0',
-    entry_point='gridworld.envs.ibc_world:IBCGoalResettingHammerWorld',
-    kwargs={'res':3, 'visible_agent':True, 'agent_centric':False, 'batch_reward':False}
+    id='Crafting-3Tasks-v0',
+    entry_point='gridworld.envs.babayai_wrapper:GymCraftingEnv',
+    kwargs={'num_tasks': 3}
+)
+register(
+    id='Crafting-4Tasks-v0',
+    entry_point='gridworld.envs.babayai_wrapper:GymCraftingEnv',
+    kwargs={'num_tasks': 4}
 )
--- crafting/gridworld/envs/babayai_wrapper.py
+++ crafting/gridworld/envs/babayai_wrapper.py
@@ -1,4 +1,4 @@
-
+import gym
 import numpy as np
 import sys
 from six import StringIO, b
@@ -6,6 +6,7 @@ import copy
 from gym import Env
 from gridworld.envs.grid_affordance import HammerWorld, OBJECTS
 from gridworld.policies.language import simple_language
+from gridworld.policies.composite_policy import CompositePolicy
 
 policy_diffs = {
     'EatBreadPolicy': {'bread': -1},
@@ -20,7 +21,7 @@ def get_true_counts_diff(policy_list):
     for obj in OBJECTS:
         for pol in policy_list:
             if obj in policy_diffs[pol]:
-                counts_diff[obj].append(policy_diffs[pol])
+                counts_diff[obj].append(policy_diffs[pol][obj])
     return counts_diff
 
 def eval_counts(ref_counts_diff, counts_diff):
@@ -55,8 +56,11 @@ class GymCraftingEnv(Env):
     def reset(self):
         instruction, policy_list = simple_language(self.num_tasks)
         policy = CompositePolicy(policy_list, self.env.action_space, self.policy_env, noise_level=0.1)
+        self.policy = policy
+        self.policy_list = policy_list
+        self.policy.reset()
         self.ref_counts_diff = get_true_counts_diff(policy_list)
-        obs = self.env.reset(min_obj = policy.min_object_nums())
+        obs = self.env.reset(min_obj = self.policy.min_object_nums())
         self.states = [copy.deepcopy(self.env.state)]
         return obs
         
@@ -66,33 +70,12 @@ class GymCraftingEnv(Env):
         object_counts = {obj: [s['object_counts'][obj] for s in self.states] for obj in OBJECTS}
         counts_diff = get_counts_diff(object_counts)
         success = eval_counts(self.ref_counts_diff, counts_diff)
+        info['oject_counts'] = counts_diff
+        info['reference_counts'] = self.ref_counts_diff
+        info['policy_list'] = self.policy_list
         if success:
             d = True
             r = 1
         return obs, r, d, info
     
     
-##################
-# Gym Registration
-##################
-
-gym.envs.registration.register(
-    id='Crafting-Atomic-v0',
-    entry_point='gridworld.envs.grid_affordance.babayai_wrapper:GymCraftingEnv',
-    kwargs={'num_tasks': 1}
-)
-gym.envs.registration.register(
-    id='Crafting-2Tasks-v0',
-    entry_point='gridworld.envs.grid_affordance.babayai_wrapper:GymCraftingEnv',
-    kwargs={'num_tasks': 2}
-)
-gym.envs.registration.register(
-    id='Crafting-3Tasks-v0',
-    entry_point='gridworld.envs.grid_affordance.babayai_wrapper:GymCraftingEnv',
-    kwargs={'num_tasks': 3}
-)
-gym.envs.registration.register(
-    id='Crafting-4Tasks-v0',
-    entry_point='gridworld.envs.grid_affordance.babayai_wrapper:GymCraftingEnv',
-    kwargs={'num_tasks': 4}
-)
